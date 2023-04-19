7386df2085a2f73825c70aada78f44ba22a7d0da
Adam La Morre <alamorre@gmail.com>
2023-04-19 13:30:30 -0700

    final edits

--- server/urls.py
+++ server/urls.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('login', views.login),
     re_path('signup', views.signup),
+    re_path('login', views.login),
     re_path('test_token', views.test_token),
 ]
--- server/views.py
+++ server/views.py
@@ -11,15 +11,6 @@ from rest_framework.authtoken.models import Token
 from .serializers import UserSerializer
 
 @api_view(['POST'])
-def login(request):
-    user = get_object_or_404(User, username=request.data['username'])
-    if not user.check_password(request.data['password']):
-        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
-    token, created = Token.objects.get_or_create(user=user)
-    serializer = UserSerializer(user)
-    return Response({'token': token.key, 'user': serializer.data})
-
-@api_view(['POST'])
 def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
@@ -30,6 +21,15 @@ def signup(request):
         token = Token.objects.create(user=user)
         return Response({'token': token.key, 'user': serializer.data})
     return Response(serializer.errors, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])

a21f6514b97dd484f3c45606a2279f1b1e74f8a1
Adam La Morre <alamorre@gmail.com>
2023-04-19 13:09:08 -0700

    concise URLs

--- server/urls.py
+++ server/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     re_path('login', views.login),
     re_path('signup', views.signup),
     re_path('ping', views.ping),
